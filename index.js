41f6e3e62ae5264c93fb9bcb097a02cb903d1f69
jrc03c <jrc03c@pm.me>
2024-01-19 20:06:34 -0600

    ...

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const { JSDOM } = require("jsdom")
+const pathJoin = require("./helpers/path-join")
 const RobotsConfig = require("./helpers/robots-config")
 
-class DomainConfig {
-  robotsConfig = null
-  urlsFromSitemaps = []
-}
-
 class WebCrawler {
-  cacheDir = null
   delay = 100
   domainConfigs = {}
   filter = () => true
@@ -19,14 +14,8 @@ class WebCrawler {
   visited = []
 
   constructor(options) {
-    this.cacheDir = options.cacheDir
     this.delay = options.delay || this.delay
     this.filter = options.filter || this.filter
-  }
-
-  clearCache() {
-    this.emit("clear-cache")
-    return this
   }
 
   emit(channel, payload) {
@@ -61,10 +50,10 @@ class WebCrawler {
     return this
   }
 
-  async start(url, robots, sitemap) {
+  async start(url) {
     // - fetch and parse robots.txt
-    // - fetch and parse sitemap.xml
-    // - add root and all pages in sitemap.xml to frontier
+    // - fetch and parse sitemap(s)
+    // - add root and all pages in sitemap(s) to frontier
     // - while the frontier has urls in it:
     //   - get a url
     //   - remove the url from the frontier and add it to the "visited" list
@@ -85,7 +74,77 @@ class WebCrawler {
     this.isPaused = false
     this.emit("start")
 
-    // ...
+    url = url instanceof URL ? url.toString() : url
+
+    // fetch and parse robots.txt from the start domain
+    await (async () => {
+      const { protocol, hostname } = new URL(url)
+      const robotsUrl = pathJoin(protocol + "//" + hostname, "robots.txt")
+      const response = await fetch(robotsUrl)
+
+      if (response.status === 200) {
+        const raw = await response.text()
+        const config = RobotsConfig.parse(raw)
+        this.domainConfigs[hostname] = config
+      } else {
+        this.emit(
+          "warn",
+          `No robots.txt file was found for the domain "${hostname}"!`,
+        )
+
+        this.domainConfigs[hostname] = new RobotsConfig()
+      }
+    })()
+
+    // fetch and parse sitemap(s)
+    await (async () => {
+      const { protocol, hostname } = new URL(url)
+      const config = this.domainConfigs[hostname]
+      const sitemapUrls = config.sitemapUrls || []
+
+      if (sitemapUrls.length === 0) {
+        sitemapUrls.push(pathJoin(protocol + "//" + hostname, "sitemap.xml"))
+        sitemapUrls.push(pathJoin(protocol + "//" + hostname, "sitemap.txt"))
+      }
+
+      for (const sitemapUrl of sitemapUrls) {
+        const response = await fetch(sitemapUrl)
+
+        if (response.status === 200) {
+          const raw = await response.text()
+
+          if (sitemapUrl.toLowerCase().endsWith(".xml")) {
+            const dom = new JSDOM(raw, { contentType: "text/xml" })
+
+            const childNodes = Array.from(
+              dom.window.document.documentElement.childNodes,
+            )
+
+            for (const child of childNodes) {
+              try {
+                const loc = child.querySelector("loc").innerHTML
+                this.frontier.push(loc)
+              } catch (e) {
+                // ...
+              }
+            }
+          }
+
+          if (sitemapUrl.toLowerCase().endsWith(".txt")) {
+            raw
+              .split("\n")
+              .map(v => v.trim())
+              .filter(v => v.length > 0)
+              .forEach(v => this.frontier.push(v))
+          }
+        } else {
+          this.emit(
+            "error",
+            `Error fetching sitemap: (${response.status}) ${sitemapUrl}`,
+          )
+        }
+      }
+    })()
 
     return this
   }
